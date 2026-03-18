f9c3991fb00a639aecaefcae0783ec5647742a7a
Anvo00 <vitoriccio03@gmail.com>
2026-03-18 11:09:21 +0100

    [ADD] Keyscrolling

--- src/main.js
+++ src/main.js
@@ -3,15 +3,14 @@ import ScrollTrigger from 'gsap/ScrollTrigger';
 import Lenis from 'lenis';
 
 document.addEventListener("DOMContentLoaded", () => {
+    history.scrollRestoration = "manual";
+    window.scrollTo(0, 0);
+
     gsap.registerPlugin(ScrollTrigger);
 
     const lenis = new Lenis();
     lenis.on("scroll", ScrollTrigger.update);
-
-    gsap.ticker.add((time) => {
-        lenis.raf(time * 750);
-    });
-
+    gsap.ticker.add((time) => { lenis.raf(time * 750); });
     gsap.ticker.lagSmoothing(0);
 
     function splitTextIntoSpans(selector) {
@@ -52,61 +51,95 @@ document.addEventListener("DOMContentLoaded", () => {
 
     splitTextIntoSpans(".mask h1");
     populateGallery();
+    ScrollTrigger.refresh();
 
+    // --- Progress bar ---
     ScrollTrigger.create({
         trigger: "body",
         start: "top top",
         end: "bottom bottom",
         onUpdate: (self) => {
-            gsap.set(".progress-bar", {
-                scaleY: self.progress
-            });
+            gsap.set(".progress-bar", { scaleY: self.progress });
         }
     });
 
     const previewImg = document.querySelector(".preview-img img");
+    const indicator = document.querySelector(".indicator");
+    const indicatorStep = 18;
+    gsap.set(indicator, { top: "0px" });
+
+    const names = gsap.utils.toArray(".name");
+    const projects = gsap.utils.toArray(".project");
     const imgElements = document.querySelectorAll(".img img");
-    imgElements.forEach((img) => {
+
+    // Dichiarate prima del loop per essere visibili a updateUI
+    let isSnapping = false;
+    let currentImgIndex = 0;
+    let isKeyScrolling = false;
+
+    imgElements.forEach((img, globalIndex) => {
+        const positionInProject = globalIndex % imagesPerProject;
+        const projectIndex = Math.floor(globalIndex / imagesPerProject);
+        const isLastInProject = positionInProject === imagesPerProject - 1;
+        const nextProject = projects[projectIndex + 1] || null;
+
+        function updateUI(idx) {
+            if (!isKeyScrolling) currentImgIndex = idx;
+
+            previewImg.src = img.src;
+
+            gsap.to(indicator, {
+                top: positionInProject * indicatorStep + "px",
+                duration: 0.3,
+                ease: "power2.out"
+            });
+
+            names.forEach((name, i) => {
+                name.classList.toggle("active", i === positionInProject);
+            });
+        }
+
         ScrollTrigger.create({
             trigger: img,
             start: "top 50%",
             end: "bottom 50%",
-            onEnter: () => {previewImg.src = img.src},
-            onEnterBack: () => {previewImg.src = img.src}
+            onEnter: () => { updateUI(globalIndex); },
+            onEnterBack: () => { updateUI(globalIndex); },
+
+            onLeave: () => {
+                if (isLastInProject && nextProject && !isSnapping) {
+                    isSnapping = true;
+
+                    const nextProjectImages = nextProject.querySelectorAll(".img img");
+                    const firstImageNextProject = nextProjectImages[0];
+
+                    lenis.scrollTo(nextProject, {
+                        offset: -(window.innerHeight * 0.48),
+                        duration: 0.5,
+                        easing: (t) => t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t,
+                        onComplete: () => {
+                            if (firstImageNextProject) {
+                                previewImg.src = firstImageNextProject.src;
+                                gsap.to(indicator, { top: "0px", duration: 0.3, ease: "power2.out" });
+                                names.forEach((name, i) => { name.classList.toggle("active", i === 0); });
+                            }
+
+                            lenis.stop();
+                            setTimeout(() => {
+                                lenis.start();
+                                setTimeout(() => { isSnapping = false; }, 100);
+                            }, 600);
+                        }
+                    });
+                }
+            }
         });
     });
 
-    const indicator = document.querySelector(".indicator");
-    const indicatorStep = 18;
-    const names = gsap.utils.toArray(".name");
-    gsap.set(indicator, {
-        top: "0px"
-    });
-
-    const projects = gsap.utils.toArray(".project");
-    projects.forEach((project, index) => {ScrollTrigger.create({
-        trigger: project,
-        start: "top 50%",
-        end: "bottom 50%",
-        onEnter: () => {
-            gsap.to(indicator, {
-                top: index * indicatorStep + "px",
-                duration: 0.3,
-                ease: "power2.out"
-            });
-            names.forEach((name, i) => {
-                name.classList.toggle("active", i === index);
-            })
-        },
-        onLeaveBack: () => {
-            gsap.to(indicator, {
-                top: (index - 1) * indicatorStep + "px",
-                duration: 0.3,
-                ease: "power2.out"
-            });
-            names.forEach((name, i) => {name.classList.toggle("active", i === index - 1)});
-        }
-    })});
+    // --- Digit animation ---
+    let scrollVelocity = 0;
+    let lastScrollTop = 0;
+    let activeIndex = -1;
 
     projects.forEach((project, i) => {
         const mask = project.querySelector(".mask");
@@ -114,19 +147,13 @@ document.addEventListener("DOMContentLoaded", () => {
         const firstDigit = digitWrapper.querySelector(".first");
         const secondDigit = digitWrapper.querySelector(".second");
 
-        gsap.set([mask, digitWrapper, firstDigit, secondDigit], {y : 0});
-        gsap.set(mask, {
-            position: "absolute",
-            top: 0
-        });
+        gsap.set([mask, digitWrapper, firstDigit, secondDigit], { y: 0 });
+        gsap.set(mask, { position: "absolute", top: 0 });
 
         ScrollTrigger.create({
             trigger: project,
             start: "top bottom",
             end: "bottom top",
-            anticipatePin: 1,
-            fastScrollEnd: true,
-            preventOverlaps: true,
             onUpdate: (self) => {
                 const projectRect = project.getBoundingClientRect();
                 const windowCenter = window.innerHeight / 2;
@@ -134,71 +161,36 @@ document.addEventListener("DOMContentLoaded", () => {
                 const velocityAdjustment = Math.min(scrollVelocity * 0.1, 100);
                 const pushPoint = window.innerHeight * (0.85 + velocityAdjustment / window.innerHeight);
 
-                if(projectRect.top < windowCenter) {
-                    if(!mask.isfixed) {
+                if (projectRect.top < windowCenter) {
+                    if (!mask.isfixed) {
                         mask.isfixed = true;
-                        gsap.set(mask, {
-                            position : "fixed",
-                            top: "50vh"
-                        });
+                        gsap.set(mask, { position: "fixed", top: "50vh" });
                     }
 
-                    if(nextProject){
+                    if (nextProject) {
                         const nextRect = nextProject.getBoundingClientRect();
-
-                        if(nextRect.top <= pushPoint && activeIndex !== i + 1) {
+                        if (nextRect.top <= pushPoint && activeIndex !== i + 1) {
                             gsap.killTweensOf([mask, digitWrapper, firstDigit, secondDigit]);
-
                             activeIndex = i + 1;
-                            gsap.to(mask, {
-                                y: -80,
-                                duration: 0.3,
-                                ease: "power2.out",
-                                overwrite: true
-                            });
 
-                            gsap.to(digitWrapper, {
-                                y: -80,
-                                duration: 0.5,
-                                delay: 0.5,
-                                ease: "power2.out",
-                                overwrite: true
-                            });
-
-                            gsap.to(firstDigit, {
-                                y: -80,
-                                duration: 0.75,
-                                ease: "power2.out",
-                                overwrite: true
-                            });
-
-                            gsap.to(secondDigit, {
-                                y: -80,
-                                duration: 0.75,
-                                delay: 0.1,
-                                ease: "power2.out",
-                                overwrite: true
-                            });
+                            gsap.to(mask, { y: -80, duration: 0.3, ease: "power2.out", overwrite: true });
+                            gsap.to(digitWrapper, { y: -80, duration: 0.5, delay: 0.5, ease: "power2.out", overwrite: true });
+                            gsap.to(firstDigit, { y: -80, duration: 0.75, ease: "power2.out", overwrite: true });
+                            gsap.to(secondDigit, { y: -80, duration: 0.75, delay: 0.1, ease: "power2.out", overwrite: true });
                         }
                     }
                 } else {
                     mask.isfixed = false;
-                    gsap.set(mask, {
-                        posiition: "absolute",
-                        top: 0
-                    });
+                    gsap.set(mask, { position: "absolute", top: 0 });
                 }
 
-                if(self.direction === -1 && projectRect.top > windowCenter) {
+                if (self.direction === -1 && projectRect.top > windowCenter) {
                     mask.isfixed = false;
-                    gsap.set(mask, {
-                        position: "absolute",
-                        top: 0
-                    });
+                    gsap.set(mask, { position: "absolute", top: 0 });
 
-                    if(i > 0 && activeIndex === i) {
+                    if (i > 0 && activeIndex === i) {
                         const prevProject = projects[i - 1];
-                        if(prevProject) {
+                        if (prevProject) {
                             const prevMask = prevProject.querySelector(".mask");
                             const prevWrapper = prevProject.querySelector(".digit-wrapper");
                             const prevFirst = prevWrapper.querySelector(".first");
@@ -207,49 +199,93 @@ document.addEventListener("DOMContentLoaded", () => {
                             gsap.killTweensOf([prevMask, prevWrapper, prevFirst, prevSecond]);
 
                             activeIndex = i - 1;
-                            gsap.to([prevMask, prevWrapper], {
-                                y: 0,
-                                duration: 0.3,
-                                ease: "power2.out",
-                                overwrite: true
-                            });
-
-                            gsap.to(prevFirst, {
-                                y: 0,
-                                duration: 0.75,
-                                ease: "power2.out",
-                                overwrite: true
-                            });
-
-                            gsap.to(prevSecond, {
-                                y: 0,
-                                duration: 0.75,
-                                delay: 0.1,
-                                ease: "power2.out",
-                                overwrite: true
-                            })
+                            gsap.to([prevMask, prevWrapper], { y: 0, duration: 0.3, ease: "power2.out", overwrite: true });
+                            gsap.to(prevFirst, { y: 0, duration: 0.75, ease: "power2.out", overwrite: true });
+                            gsap.to(prevSecond, { y: 0, duration: 0.75, delay: 0.1, ease: "power2.out", overwrite: true });
                         }
                     }
                 }
             },
 
-            onEnter: () => {
-                if(i === 0) activeIndex = 0;
-            }
+            onEnter: () => { if (i === 0) activeIndex = 0; }
         });
     });
-
-    let activeIndex = -1;
-    let lastScrollTop = 0;
-    let scrollVelocity = 0;
 
     window.addEventListener("scroll", () => {
         const st = window.pageYOffset;
         scrollVelocity = Math.abs(st - lastScrollTop);
         lastScrollTop = st;
-    },
-    {
-        passive: true
+    }, { passive: true });
+
+    // --- Navigazione con frecce ---
+    const allImgs = Array.from(document.querySelectorAll(".img img"));
+    let currentImageIndex = -1; // ← -1 così il primo press va all'immagine 0
+
+    function scrollToImage(index) {
+        if (index < 0 || index >= allImgs.length || isKeyScrolling) return;
+
+        isKeyScrolling = true;
+        currentImageIndex = index;
+
+        const safetyTimeout = setTimeout(() => { isKeyScrolling = false; }, 800); // massimo atteso
+
+        const targetImg = allImgs[index];
+        const positionInProject = index % imagesPerProject;
+        const isProjectChange = positionInProject === 0 && index !== 0;
+
+        lenis.start();
+
+        lenis.scrollTo(targetImg, {
+            offset: -(window.innerHeight / 2) + (targetImg.offsetHeight / 2),
+            duration: isProjectChange ? 0.4 : 0.22,
+            easing: isProjectChange
+                ? (t) => t < 0.5 ? 4 * t * t * t : 1 - Math.pow(-2 * t + 2, 3) / 2
+                : (t) => t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t,
+
+            onComplete: () => {
+                clearTimeout(safetyTimeout);
+                previewImg.src = targetImg.src;
+                gsap.to(indicator, {
+                    top: positionInProject * indicatorStep + "px",
+                    duration: 0.3,
+                    ease: "power2.out"
+                });
+                names.forEach((name, i) => {
+                    name.classList.toggle("active", i === positionInProject);
+                });
+
+                // Pausa più lunga se cambia progetto, senza usare lenis.stop()
+                // che bloccherebbe i successivi scrollToImage
+                const pause = isProjectChange ? 200 : 60;
+                setTimeout(() => { isKeyScrolling = false; }, pause);
+            }
+        });
     }
-    );
+
+    function getCurrentImageIndex() {
+        const center = window.innerHeight / 2;
+        let closest = 0;
+        let closestDist = Infinity;
+        allImgs.forEach((img, i) => {
+            const rect = img.getBoundingClientRect();
+            const dist = Math.abs((rect.top + rect.height / 2) - center);
+            if (dist < closestDist) {
+                closestDist = dist;
+                closest = i;
+            }
+        });
+        return closest;
+    }
+
+    document.addEventListener("keydown", (e) => {
+        if (e.key === "ArrowDown" || e.key === "ArrowRight") {
+            e.preventDefault();
+            if (!isKeyScrolling) currentImgIndex = getCurrentImageIndex(); // ← risincronizza
+            scrollToImage(currentImgIndex + 1);
+        } else if (e.key === "ArrowUp" || e.key === "ArrowLeft") {
+            e.preventDefault();
+            if (!isKeyScrolling) currentImgIndex = getCurrentImageIndex(); // ← risincronizza
+            scrollToImage(currentImgIndex - 1);
+        }
+    });
 });
